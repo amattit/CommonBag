ec96672042535dedd96c48c4191f370c2bda54ea
Mikhail <_silo@mail.ru>
2022-06-16 23:55:53 +0300

    Initial Commit

---
+++ CommonBag/CommonBagApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommonBagApp.swift
+//  CommonBag
+//
+//  Created by MikhailSeregin on 16.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct CommonBagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
