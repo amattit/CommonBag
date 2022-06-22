80b0de21c725a4843fd3a5e729370e5b2f834436
Mikhail <_silo@mail.ru>
2022-06-22 23:26:24 +0300

    suggests

--- CommonBag/Screens/ProductSuggest/ProductSuggestView.swift
+++ CommonBag/Screens/ProductSuggest/ProductSuggestView.swift
@@ -5,8 +5,10 @@
 //  Created by MikhailSeregin on 22.06.2022.
 //
 
+// TODO: подумать, как лучше сделать
 import SwiftUI
 import Stinsen
+import Combine
 
 struct ProductSuggestView: View {
     @ObservedObject var viewModel: ProductSuggestViewModel
@@ -17,21 +19,38 @@ struct ProductSuggestView: View {
 
 final class ProductSuggestViewModel: ObservableObject {
     let networking: NetworkClientProtocol
-    init(networking: NetworkClientProtocol) {
+    let searchPublisher: PassthroughSubject<String, Never>
+    private var disposables = Set<AnyCancellable>()
+    init(networking: NetworkClientProtocol, searchPublisher: PassthroughSubject<String, Never>) {
+        self.searchPublisher = searchPublisher
         self.networking = networking
+        bind()
+    }
+    
+    private func bind() {
+        searchPublisher
+            .debounce(for: .seconds(1.5), scheduler: RunLoop.main)
+            .sink(receiveValue: { searchValue in
+                print(searchValue)
+            })
+            .store(in: &disposables)
+
     }
 }
 
 final class ProductSuggestCoordinator: NavigationCoordinatable {
     let stack = NavigationStack(initial: \ProductSuggestCoordinator.start)
-    let networking: NetworkClientProtocol
     @Root var start = makeStart
+
+    let networking: NetworkClientProtocol
+    let searchPublisher: PassthroughSubject<String, Never>
     
-    init(networking: NetworkClientProtocol) {
+    init(networking: NetworkClientProtocol, searchPublisher: PassthroughSubject<String, Never>) {
         self.networking = networking
+        self.searchPublisher = searchPublisher
     }
 
     @ViewBuilder func makeStart() -> some View {
-        ProductSuggestView(viewModel: .init(networking: networking))
+        ProductSuggestView(viewModel: .init(networking: networking, searchPublisher: searchPublisher))
     }
 }
